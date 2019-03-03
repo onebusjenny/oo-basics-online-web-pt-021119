71708246369800e6cd0250366c0c60fda6a72a97
Jenny Chu <jchudesign0930@gmail.com>
2019-03-03 02:49:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,25 @@
 class Book
+  
+  def initialize (title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  
+  def author= (author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  
+  def page_count= (page_count)
+    @page_count = page_count
 end
+
 
